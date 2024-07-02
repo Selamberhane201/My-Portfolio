b494b54e41a55cba33ff7d31d588766354078b23
Selamberhane201 <101728116+Selamberhane201@users.noreply.github.com>
2024-07-02 10:04:09 -0400

    Add files via upload

---
+++ photo.js
@@ -0,0 +1,68 @@
+function check_value(imagevalue) {
+    switch (imagevalue) {
+        case 1:
+            let x = document.getElementById("photo");
+            x.src = 'image/photo two.jpg';
+            x.style.width = "100%";
+            // x.style.animationName = "example"
+            // x.style.animationDuration = "3s";
+            break;
+        case 2:
+            let y = document.getElementById("photo");
+            y.src = 'image/photoOne.jpg';
+            y.style.width = "100%";
+            y.style.animationName = "example"
+            y.style.animationDuration = "3s";
+            break;
+        case 3:
+            let c = document.getElementById("photo");
+            c.src = 'image/photoFour.jpg';
+            c.style.width = "100%";
+            break;
+        case 4:
+            let d = document.getElementById("photo");
+            d.src = 'image/photoThree.JPG';
+            d.style.width = "100%";
+            break;
+
+        case 5:
+            let e = document.getElementById("photo");
+            e.src = 'image/w1.jpg';
+            e.style.width = "100%";
+            break;
+
+        case 6:
+            let f = document.getElementById("photo");
+            f.src = 'image/W5.jpeg';
+            f.style.width = "100%";
+            break;
+
+        case 7:
+            let g = document.getElementById("photo");
+            g.src = 'image/w3.jpg';
+            g.style.width = "100%";
+            break;
+
+        case 8:
+            let h = document.getElementById("photo");
+            h.src = 'image/W6.jpeg';
+            h.style.width = "100%";
+            break;
+
+        case 9:
+            let l = document.getElementById("photo");
+            l.src = 'image/W10.jpeg';
+            l.style.width = "100%";
+            break;
+        case 10:
+            let m = document.getElementById("photo");
+            m.src = 'image/W9.jpeg';
+            m.style.width = "100%";
+            break;
+
+
+    }
+
+}
+
+
---
+++ retouch.js
@@ -0,0 +1,53 @@
+function check_value(imagevalue) {
+    switch (imagevalue) {
+        case 1:
+            let x = document.getElementById("photo");
+            x.src = 'image/retouch1.jpg';
+            x.style.width = "100%";
+            // x.style.animationName = "example"
+            // x.style.animationDuration = "3s";
+            break;
+        case 2:
+            let y = document.getElementById("photo");
+            y.src = 'image/retouch2.jpg';
+            y.style.width = "100%";
+            y.style.animationName = "example"
+            y.style.animationDuration = "3s";
+            break;
+        case 3:
+            let c = document.getElementById("photo");
+            c.src = 'image/retouch3.jpg';
+            c.style.width = "100%";
+            break;
+        case 4:
+            let d = document.getElementById("photo");
+            d.src = 'image/retouch4.jpg';
+            d.style.width = "100%";
+            break;
+        case 5:
+            let e = document.getElementById("photo");
+            e.src = 'image/retouch7.jpg';
+            e.style.width = "100%";
+            break;
+
+        case 6:
+            let f = document.getElementById("photo");
+            f.src = 'image/retouch6.jpg';
+            f.style.width = "100%";
+            break;
+
+        case 7:
+            let g = document.getElementById("photo");
+            g.src = 'image/retouch5.jpg';
+            g.style.width = "100%";
+            break;
+
+        case 8:
+            let h = document.getElementById("photo");
+            h.src = 'image/retouch8.jpg';
+            h.style.width = "100%";
+            break;
+
+    }
+
+}
